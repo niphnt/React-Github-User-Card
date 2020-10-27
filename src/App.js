7209e2b99c5c52dc0bd61784aa518d9ec25c6c3b
Joe L <joseph.lasata@gmail.com>
2020-10-27 15:18:25 -0500

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import User from './User';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    userInfo: {}
+  }
+
+  componentDidMount() {
+    // console.log("WE MOUNTED")
+    axios.get(`https://api.github.com/users/ladrillo`)
+      .then(res => {
+        this.setState({
+          userInfo: res.data
+        })
+        console.log(this.state.userInfo)
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Git User Card</h1>
+        <User userInfo={this.state.userInfo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// API GET URL: https://api.github.com/users/ladrillo
